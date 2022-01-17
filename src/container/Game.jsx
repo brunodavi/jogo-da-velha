e2d31d254aca242071c3e23cbe52c62b488f955e
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2022-01-17 19:42:35 -0300

    "_" --> " "

--- src/container/Game.jsx
+++ src/container/Game.jsx
@@ -1,28 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 function Game() {
-  const [inputs, setInputs] = useState([
-    ['_', '_', '_'],
-    ['_', '_', '_'],
-    ['_', '_', '_'],
-  ]);
-
-  // useEffect(null, [inputs]);
+  const array = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+  ]
+  const [inputs, setInputs] = useState(array);
+  const [player, setPlayer] = useState('P1');
 
   const handleClick = (i1, i2, value) => {
     let arr = inputs;
-    console.log(arr);
-    if (arr[i1][i2] === '_') {
+    if (arr[i1][i2] === ' ') {
       arr[i1][i2] = value;
       setInputs(arr);
     }
   }
 
+  const click = () => {
+    const players = {
+      P1: 'X',
+      P2: 'O'
+    };
+
+    const val = players[player];
+    setPlayer(player === 'P1' ? 'P2' : 'P1');
+    return val;
+  }
+
+  const restart = () => setInputs(array);
+
   const table = inputs.map((_, i1) => (
-  <div>
+  <div key={ i1 }>
     {
       inputs[i1].map((value, i2) => (
-        <input type="text" value={ value } onClick={ () => handleClick(i1, i2, value) } />
+        <input key={ i2 } type="button" value={ value } onClick={ () => handleClick(i1, i2, click()) } />
       ))
     }
   </div>
@@ -31,9 +43,8 @@ function Game() {
   return ( 
     <main>
       { table }
-      <input type="button" value="Reiniciar" />
+      <input type="button" id="reiniciar" value="Reiniciar" onClick={ restart } />
     </main>
-
   );
 }
 
