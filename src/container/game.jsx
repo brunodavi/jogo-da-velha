d2fcd14980a0fb5e7574c3358506585a04e0e9d5
Junior030 <juniorhenrique030@gmail.com>
2022-01-17 18:58:39 -0300

    Logica do tabuleiro v1.0

--- src/container/game.jsx
+++ src/container/game.jsx
@@ -1,26 +1,44 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 
 
 function Game() {
-  const [inputs, setInputs] = useState("-")
+  const [jogador, setJogador] = useState(true)
+  const [tabuleiro, setTabuleiro] = useState([['-','-','-'],['-','-','-'],['-','-','-']])
+
+  const restart = () => {
+    setJogador(true)
+    window.location.reload()
+  }
+
+  const suavez = ({target}) => {
+    let casa = document.getElementById(target.id).innerText
+    if (casa === '-') {
+      jogador ? casa = "X" 
+      : casa = "O"
+      let newTabuleiro = tabuleiro
+      newTabuleiro[Number(target.id[0])][Number(target.id[3])] = casa
+      setTabuleiro(newTabuleiro)
+      setJogador(!jogador)   
+    }
+  }
+
+
   return ( 
     <main>
-      <div>
-        <input type="button" value="_" />
-        <input type="button" value="_" /> 
-        <input type="button" value="_" />
-      </div>
-      <div>
-        <input type="button" value="_"/>
-        <input type="button" value="_"/>
-        <input type="button" value="_"/>
-      </div>
-      <div>
-        <input type="button" value="_"/>
-        <input type="button" value="_"/>
-        <input type="button" value="_"/>
-      </div>
-      <input type="button" value="Reiniciar" id="reiniciar" />
+    {      
+      tabuleiro.map((linha, indexL) => (
+        <div key={indexL}>
+          {linha.map((coluna, indexC) =>(
+            <button key={`${indexL}, ${indexC}`} id={`${indexL}, ${indexC}`} type="button" onClick={suavez}>
+              {coluna}
+            </button>
+          ))}
+        </div>
+      ))   
+    }
+      <button type="button" id="reiniciar" onClick={restart}>
+        Reiniciar
+      </button>
     </main>
 
   );
