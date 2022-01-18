d491342da6e0811982170a98a51c6c3a13effb13
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2022-01-18 03:49:11 -0300

    Add Winner

--- src/container/Game.jsx
+++ src/container/Game.jsx
@@ -1,50 +1,85 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Game() {
   const array = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-  ]
-  const [inputs, setInputs] = useState(array);
-  const [player, setPlayer] = useState('P1');
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+  ];
 
-  const handleClick = (i1, i2, value) => {
+  const macros = [
+    [ [0, 0], [0, 1], [0, 2] ],
+    [ [1, 0], [2, 1], [1, 2] ],
+    [ [2, 0], [2, 1], [2, 2] ],
+    [ [0, 0], [1, 1], [2, 2] ],
+    [ [0, 2], [1, 1], [2, 0] ]
+  ];
+
+  const [inputs, setInputs] = useState(array);
+  const [winner, setWinner] = useState(false);
+  const [first, setFirst] = useState(true);
+
+  const allEqual = (arr) => (
+    arr.every((val) => arr[0] === val)
+  );
+
+  const getChar = () => (
+    (first) ? 'X' : 'O'
+  )
+
+  function checkWinner(arr = inputs) {
+    const result = macros
+      .some(([[i0, i1], [i2, i3], [i4, i5]]) => (
+        allEqual(
+          [arr[i0][i1], arr[i2][i3], arr[i4][i5]]
+        )
+      ));
+    
+    const char = getChar();
+    setWinner(result && char);
+  }
+
+  function handleClick(i0, i1, value) {
     let arr = inputs;
-    if (arr[i1][i2] === ' ') {
-      arr[i1][i2] = value;
+    if (!winner && arr[i0][i1] === " ") {
+      arr[i0][i1] = value;
       setInputs(arr);
+      checkWinner();
     }
   }
 
-  const click = () => {
-    const players = {
-      P1: 'X',
-      P2: 'O'
-    };
-
-    const val = players[player];
-    setPlayer(player === 'P1' ? 'P2' : 'P1');
+  function click() {
+    const val = getChar() ;
+    setFirst(!first);
     return val;
   }
 
   const restart = () => setInputs(array);
 
   const table = inputs.map((_, i1) => (
-  <div key={ i1 }>
-    {
-      inputs[i1].map((value, i2) => (
-        <input key={ i2 } type="button" value={ value } onClick={ () => handleClick(i1, i2, click()) } />
-      ))
-    }
-  </div>
+    <div key={ i1 }>
+      {inputs[i1].map((value, i2) => (
+        <input
+          key={ i2 }
+          type="button"
+          value={ value }
+          onClick={ () => handleClick(i1, i2, click()) }
+        />
+      ))}
+    </div>
   ));
 
-  return ( 
-    <main>
+  if (winner) return (
+    <div>
+      <h1>{ `${winner} é o vencedor` }</h1>
+    </div>
+  );
+
+  return (
+    <div>
       { table }
-      <input type="button" id="reiniciar" value="Reiniciar" onClick={ restart } />
-    </main>
+      <input type="button" id="reiniciar" value="Reiniciar" onClick={restart} />
+    </div>
   );
 }
 
